94ab57ea21f7ab794298c21e3328cb5740cf3a91
Diogo Enes <65285981+Dycool@users.noreply.github.com>
2026-09-05 14:18:13 +0100

    build: embed explicit Windows application manifest

--- build.rs
+++ build.rs
@@ -1,3 +1,34 @@
+const WINDOWS_MANIFEST: &str = r#"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <assemblyIdentity
+      version="1.0.0.0"
+      processorArchitecture="amd64"
+      name="Dycool.CorsairEliteDisplay"
+      type="win32" />
+  <description>Corsair Elite Display</description>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="asInvoker" uiAccess="false" />
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+  <compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1">
+    <application>
+      <supportedOS Id="{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}" />
+    </application>
+  </compatibility>
+  <application xmlns="urn:schemas-microsoft-com:asm.v3">
+    <windowsSettings>
+      <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true/pm</dpiAware>
+      <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>
+      <longPathAware xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">true</longPathAware>
+    </windowsSettings>
+  </application>
+</assembly>
+"#;
+
 fn main() {
     println!("cargo:rerun-if-changed=assets/icon-on.ico");
     println!("cargo:rerun-if-changed=assets/icon-off.ico");
@@ -5,6 +36,7 @@ fn main() {
         let version = std::env::var("CARGO_PKG_VERSION").unwrap_or_else(|_| "1.0.0".into());
         let mut resource = winresource::WindowsResource::new();
         resource
+            .set_manifest(WINDOWS_MANIFEST)
             .set_icon_with_id("assets/icon-on.ico", "1")
             .set_icon_with_id("assets/icon-off.ico", "2")
             .set("CompanyName", "Dycool")
